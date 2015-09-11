2e513cda8f8a344c16bf712e3d62ca59ed4126ea
Kenny Grage <student@epicodus.com>
2015-09-11 10:21:53 -0700

    finish webpage functionality

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,37 @@ function phraseChanger(phrase, word_to_remove, word_to_add) {
   //debugger;
   var regex = new RegExp(word_to_remove, "gi");
   var new_phrase = phrase.replace(regex, word_to_add);
-  console.log(new_phrase);
   return new_phrase;
 
 } //End phraseChanger()
+
+$(document).ready(function() {
+  $("form#changer").submit(function(event) {
+
+      //get the user input
+      var phrase = $("input#phrase").val();
+      var word_to_remove = $("input#word_to_remove").val();
+      var word_to_add = $("input#word_to_add").val();
+
+      //get the new phrase from the function
+      var new_phrase = phraseChanger(phrase, word_to_remove, word_to_add);
+
+      //empty the textboxes so the user can input again if they want to
+      $("input#phrase").val('');
+      $("input#word_to_remove").val('');
+      $("input#word_to_add").val('');
+
+      //clear old values, if any, from new_phrase
+      $(".new_phrase").empty();
+
+      //add new value into new_phrase
+      $(".new_phrase").text(new_phrase);
+
+      //show showme incase this is the first time the user clicked the submit button
+      $(".showme").show();
+
+      //I am not sure what this does, but I am supposed to put it here
+      event.preventDefault();
+
+  }); //End submit event
+}); //End document.ready
