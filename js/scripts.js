afd39ad958af87eea21e849cc3613143b7567ecf
Kenny Grage <student@epicodus.com>
2015-09-11 09:10:48 -0700

    one test passes

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,16 @@
 function phraseChanger(phrase, word_to_remove, word_to_add) {
+  //debugger;
   phrase = phrase.replace(/[.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"");  //removing punctuation
-  var phrase_array = phrase.split(' ');                   //split the words by each space
+  var phrase_array = phrase.split(' ');                 //split the words by each space
 
   for (var i = 0; i < phrase_array.length; i++) {
     if (wordChecker(phrase_array[i]), word_to_remove) {   //if this is the word we want to change
       phrase_array[i] = word_to_add;
     }
   } //End for loop
+  var new_phrase = "" + phrase_array;                     //for some reason, .toString() didn't work. Thank you, Professor Google, for the new way
+
+  return new_phrase;
 
 } //End phraseChanger()
 
